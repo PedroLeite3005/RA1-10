80ee3e82eed42074f524055e6390ae21d57711ba
Felipe Augusto <felipe.baleche2@gmail.com>
2026-03-12 20:25:21 -0300

    adicionado código de abertura de arquivo

--- main.py
+++ main.py
@@ -5,9 +5,16 @@
 
 # Grupo: RA1 10
 
+import sys 
+
 def main():
-    pass
+    try:
+        file_path = sys.argv[1]
+        file_content = open(file_path, "r").readlines()
+    except IndexError:
+        print(f"Usage: {sys.argv[0]} [path-to-file]")
+    except FileNotFoundError:
+        print("Error: the provided file could not be opened or does not exist")
 
-
-if __name__ == "main":
+if __name__ == "__main__":
     main()
